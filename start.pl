9e5840f625d8c91083e5b55a948d85f3ace50335
Георгий Алексеев <alexeew.georgiy@gmail.com>
2014-05-13 00:22:03 +0600

    Доделал пример

--- start.pl
+++ start.pl
@@ -1,4 +1,8 @@
 use Mojo::Base -base;
+use Mojo;
+use Term::ANSIColor;
+
+++$|;
 
 opendir D, 'Exploit' or die;
 my @modules = map {s/\.pm$//; $_} grep {/^[^\.]/ && -f "Exploit/$_"} readdir D;
@@ -8,7 +12,11 @@ my @addresses;
 open F, 'addresses' or die;
 while (<F>) {
 	chomp;
-	push @addresses, $_;
+	my $address = $_;
+	$address = 'http://'.$address unless ($address =~ /^http/);
+	$address = Mojo::URL->new($address);
+	$address->path('/') if ($address->path eq '');
+	push @addresses, $address;
 }
 close F;
 
@@ -18,10 +26,9 @@ for my $module (@modules) {
 		print "ERROR: No such module [$module]\n";
 		next;
 	}
+	print "\n$module\n";
 	for my $address (@addresses) {
-		$address = 'http://'.$address unless ($address =~ /^http/);
-		$address = Mojo::URL->new($address);
-		$address->path('/') if ($address->path eq '');
-		print "$address : " . run($address) . "\n";
+		my $result = run($address) ? 'TRUE' : 'false';
+		print "	$address : $result\n";
 	}
 }
